86e42075d7f2e404351341c1e1003db8e6a5311d
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-06-01 01:02:33 +0530

    create new post feature complete

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongodb = require('mongodb');
 
 const db = require('../data/database');
 
+const ObjectId = mongodb.ObjectId;
 const router = express.Router();
 
 // Root route - redirect to home page
@@ -22,6 +24,40 @@ router.get('/new-post', async function (req, res) {
   } catch (err) {
     console.error(err);
     render('500');
+  }
+});
+
+// Handle new post form submit request
+router.post('/posts', async (req, res) => {
+  const authorId = new ObjectId(req.body.author);
+  let author;
+
+  try {
+    author = await db.getDB().collection('authors').findOne({
+      _id: authorId,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.render('500');
+  }
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  try {
+    await db.getDB().collection('posts').insertOne(newPost);
+    res.redirect('/posts');
+  } catch (err) {
+    res.render('500');
   }
 });
 
