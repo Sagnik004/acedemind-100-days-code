f896040ed1c7a64ec233b8ed6cd018417139a428
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-06-05 03:14:28 +0530

    added logic for db entry

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const multer = require('multer');
 
+const db = require('../data/database');
+
 const storageConfig = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, 'images');
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
-  }
+  },
 });
 
 const router = express.Router();
@@ -21,14 +23,20 @@ router.get('/new-user', (req, res) => {
   res.render('new-user');
 });
 
-router.post('/profiles', upload.single('image'), (req, res) => {
+router.post('/profiles', upload.single('image'), async (req, res) => {
   const uploadedImageFile = req.file;
   const userData = req.body;
 
-  console.log(uploadedImageFile);
-  console.log(userData);
+  try {
+    await db.getDb().collection('users').insertOne({
+      name: userData.username,
+      imagePath: uploadedImageFile.path,
+    });
 
-  res.redirect('/');
+    res.redirect('/');
+  } catch (err) {
+    res.status(500).send('Something went wrong!');
+  }
 });
 
 module.exports = router;
