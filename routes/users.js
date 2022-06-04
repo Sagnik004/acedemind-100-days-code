1b59291640e5fdad9cbd574ecd0a0c5ceb9641fa
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-06-05 02:26:14 +0530

    included multer for parsing file uploads

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const multer = require('multer');
 
 const router = express.Router();
+const upload = multer({  });
 
 router.get('/', (req, res) => {
   res.render('profiles');
@@ -8,6 +10,14 @@ router.get('/', (req, res) => {
 
 router.get('/new-user', (req, res) => {
   res.render('new-user');
+});
+
+router.post('/profiles', upload.single('image'), (req, res) => {
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+  console.log('uploadedImageFile: ', uploadedImageFile);
+  console.log('userData: ', userData);
+  res.send('success');
 });
 
 module.exports = router;
