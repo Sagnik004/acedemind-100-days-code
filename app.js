96b8dc34f4c4bd3871f87d81b56bef1f9bcf2551
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2022-06-19 22:00:41 +0530

    rendering first view for sign-up

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const path = require('path');
+
 const express = require('express');
 
 const authRoutes = require('./routes/auth.routes');
 
+// Create Express app
 const app = express();
+
+// Make Express aware we will use EJS view template & engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(authRoutes);
 
-app.listen(3000, () => console.log('App started successfully'));
+app.listen(3000, () => {
+  console.log(`App started successfully at port 3000...`);
+});
