de383ee8efb7ca11fff7daec609161dad765797c
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-06-28 01:39:02 +0530

    added save product to db functionality

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product.model');
+
 const getProducts = (req, res) => {
   res.render('admin/products/all-products');
 };
@@ -6,11 +8,24 @@ const getNewProduct = (req, res) => {
   res.render('admin/products/new-product');
 };
 
-const createNewProduct = (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+const createNewProduct = async (req, res, next) => {
+  try {
+    const productData = {
+      title: req.body.title,
+      summary: req.body.summary,
+      price: req.body.price,
+      description: req.body.description,
+      image: req.file.filename,
+    };
+    const product = new Product(productData);
+    await product.save();
 
-  res.redirect('/admin/products');
+    res.redirect('/admin/products');
+  } catch (error) {
+    console.error(error);
+    next(error);
+    return;
+  }
 };
 
 module.exports = {
